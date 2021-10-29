a07381f34eb9df0b38a428c9e04e0e817de0bc30
AlexSabur <alexsabur@live.ru>
2021-10-29 07:12:27 +0000

    init

---
+++ app/Commands/Play.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class Play extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'play';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
